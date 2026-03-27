a447ef448fca20a975b4d3ea7f00a6f5ff9e2539
wernher-camp <158773287+wernher-camp@users.noreply.github.com>
2026-03-27 16:18:30 -0600

    star

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Welcome to Flutte',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+return Scaffold(
+  body: Center( // Esto expande el espacio y centra al hijo
+    child: Text('Hello World',
+    style: TextStyle(
+      fontSize: 32.0,
+      color:Color.fromARGB(255, 190, 44, 44)
+    )),
+  ),
+);
+  }
+}
